2dcd8410355113361967188a24d0d03703ae9751
camila2911 <kmillaana29@gmail.com>
2022-09-27 18:11:14 -0300

    First commit

---
+++ cicd/index.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const port=process.env.PORT || 8000;
+const app=express();
+
+app.get('/',(req,res) => {
+    res.send('Hello World');
+});
+
+app.listen(port,()=> {console.log('Our app is up and runing"');});
+module.exports=app
